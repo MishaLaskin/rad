9a4ff895f226d3cef8e93c30161f19397148dffa
MishaLaskin <laskin.misha@gmail.com>
2020-04-10 18:55:29 -0700

    np save

--- train.py
+++ train.py
@@ -117,7 +117,7 @@ def evaluate(env, agent, video, num_episodes, L, step, args):
         L.log('eval/' + prefix + 'mean_episode_reward', mean_ep_reward, step)
         L.log('eval/' + prefix + 'best_episode_reward', best_ep_reward, step)
 
-        filename = args.work_dir + '/'  + exp_name + 'eval_scores.npy'
+        filename = args.work_dir + '/'  + exp_name + '/'+ args.domain_name + '-'+args.task_name + '-s' + str(args.seed) + '-eval_scores.npy'
         key = args.data_augs
         try:
             log_data = np.load(filename,allow_pickle=True)
